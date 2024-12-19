69b54f5a5b134043f518585b38b1b7acdfdc0370
ouijan <tobyharris93@gmail.com>
2024-12-19 14:10:53 +1100

    cleanup

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -69,6 +69,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		-- Enable Telescope extensions if they are installed
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
+		pcall(require("telescope").load_extension, "lazygit")
 
 		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
